9b3e7f1eac5c091b9c2b5a541f41937e6d1e6165
Olexandr Goloshchapov <olexandr.goloshchapov@oit.edu>
2021-10-27 07:42:59 -0700

    Added bare asp core api and updated Vue routing to communicate.

--- factory/vue.config.js
+++ factory/vue.config.js
@@ -1,9 +1,17 @@
 module.exports = {
     devServer: {
       open: process.platform === 'darwin',
+      // target: 'https://localhost:3000',
       host: '0.0.0.0',
       port: 3000, // CHANGE YOUR PORT HERE!
       https: true,
       hotOnly: false,
+      proxy: {
+        '^/api': {
+          target: 'https://localhost:5001',
+          ws: true,
+          changeOrigin: true
+        },
+      },
     },
   }
